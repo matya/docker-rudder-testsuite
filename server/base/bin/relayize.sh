645ff55340ee7518baf231c050ebd3f55976e7a0
Janos Mattyasovszky <mail@matya.hu>
2015-10-09 22:08:06 +0200

    Removing exit-on-error and sleep from relayize worker

--- server/base/bin/relayize.sh
+++ server/base/bin/relayize.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 exec 2>&1
 
 UUID="$1"
@@ -8,11 +7,9 @@ echo "Promoting node to relay: $UUID"
 
 echo "Forcing inventory processing:"
 /var/rudder/cfengine-community/bin/cf-agent -KI -b sendInventoryToCmdb | awk -F '@#' '/root-distributePolicy/  {print $NF}'
-sleep 1
 
 echo "Accepting node:"
 bash /rudder/bin/accept_pending.sh $UUID
-sleep 1
 
 echo "Promote to relay:"
 /opt/rudder/bin/rudder-node-to-relay $UUID
