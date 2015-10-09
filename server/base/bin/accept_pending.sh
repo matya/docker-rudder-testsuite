13754cda969ade35690d765943d33b3f1349f427
Janos Mattyasovszky <mail@matya.hu>
2015-10-09 22:21:14 +0200

    Reworked accept_pending to wait for node to appear in API

--- server/base/bin/accept_pending.sh
+++ server/base/bin/accept_pending.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+BASEURL="http://localhost:8080/rudder/api/latest"
 NODEID="$1"
 exec 2>&1
 
@@ -7,14 +8,29 @@ exec 2>&1
 API_KEY="$(</rudder/api-token)"
 
 [[ -x /usr/bin/curl ]] || { echo "No curl binary found"; exit 1; }
+CURL="/usr/bin/curl --silent -L -H 'X-API-Token: $API_KEY'"
+api() {
+    local p="$1"; shift;
+    $CURL "${BASEURL}/$p" $@
+}
 
-if  /usr/bin/curl --silent -L -H "X-API-Token: $API_KEY"           "http://localhost:8080/rudder/api/latest/nodes/$NODEID?prettify=true" | grep -q '"status": "pending"'; then
-    echo -n "** Accepting pending node $NODEID: "
-    /usr/bin/curl --silent -L -H "X-API-Token: $API_KEY" -X 'POST' "http://localhost:8080/rudder/api/latest/nodes/pending/$NODEID?prettify=true" -d "status=accepted" | \
-        grep -e '"result"'
-    exit $?
-else
-    echo "Node not found or not pending: $NODEID"
-    exit 1
-fi
+echo "- Waiting for node $NODEID to appear..."
+C=0
+while :; do
+    STATUS=$( api "nodes/$NODEID?prettify=true" | awk -F'"' '/"status":/ { print $(NF-1); exit; }' )
+    if [[ x"$STATUS" = x"pending" ]]; then
+        echo -n "- Accepting pending node $NODEID: "
+        api "nodes/pending/$NODEID?prettify=true" -X 'POST' -d "status=accepted" | \
+            grep -e '"result"'
+        exit 0
+    elif [[ x"$STATUS" = x"accepted" ]]; then
+        echo "- Node already accepted"
+        exit 0
+    fi
+    if (( C++ > 180 )); then
+        echo "- Timeout of 15 minutes reached!"
+        exit 1
+    fi
+    sleep 5
+done
 
