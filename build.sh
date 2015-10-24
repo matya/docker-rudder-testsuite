cb81ca86309e70d08238cb6f299cfe69acfbef15
Janos Mattyasovszky <mail@matya.hu>
2015-10-25 00:55:37 +0200

    Changing build.sh's invocation to follow run.sh's

--- build.sh
+++ build.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-: ${IMAGEBASE:=matya/rudder-testsuite}
-: ${VERSION:=latest}
+: ${RTS_IMAGE:=matya/rudder-testsuite}
+: ${RTS_RELEASE:=latest}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --release|-R)
+            grep -q '^-' <<<"$2" && { echo "$1 requires an argument"; exit 1; }
+            RTS_RELEASE="$2";
+            shift;
+            ;;
+        --image|-I)
+            grep -q '^-' <<<"$2" && { echo "$1 requires an argument"; exit 1; }
+            RTS_IMAGE="$2";
+            shift;
+            ;;
+        --help|-h)
+            echo "
+Usage: $0 
+    [--image|-I]   <image>      Docker image name, default: ${RTS_IMAGE}
+    [--release|-R] <release>    Rudder release to build as tag of <image>, default: ${RTS_RELEASE}.
+"
+            exit 0
+            ;;
+        *)
+            echo "-> Unknown parameter: $1. See $0 --help for usage"
+            exit 1
+            ;;
+    esac
+    shift
+done   
 
 set -e
 
@@ -10,14 +38,14 @@ for T in \
     dns \
     base \
     client.base relay.base server.base \
-    client.${VERSION} relay.${VERSION} server.${VERSION}; do
+    client.${RTS_RELEASE} relay.${RTS_RELEASE} server.${RTS_RELEASE}; do
         if grep -F -q '.' <<<"$T"; then
             DIR="${T%%.*}/${T#*.}"
         else
             DIR="$T"
         fi
         [[ -e ${DIR}/Dockerfile.internal ]] && DF="Dockerfile.internal" || DF="Dockerfile"
-        ( set -o pipefail; cd ${DIR}; docker build -t ${IMAGEBASE}:${T} -f $DF .  | strings | sed "s|^|$T>> |g" );
+        ( set -o pipefail; cd ${DIR}; docker build -t ${RTS_IMAGE}:${T} -f $DF .  | awk '{ print "'$T'>> " $0; fflush(); }'; )
         echo "=================================================================";
 done
 echo ""
