c6bd856938e3e40048d3fbdf2e42b1ef2c7aacb6
Janos Mattyasovszky <mail@matya.hu>
2015-10-24 13:44:33 +0200

    Changed output syntax

--- server/base/bin/relayize.sh
+++ server/base/bin/relayize.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-exec 2>&1
-
 UUID="$1"
-echo "Promoting node to relay: $UUID"
+echo "[relayize:$$]** Promoting node to relay: $UUID"
 
-echo "Forcing inventory processing:"
+echo "[relayize:$$] - Forcing inventory processing:"
 /var/rudder/cfengine-community/bin/cf-agent -KI -b sendInventoryToCmdb | awk -F '@#' '/root-distributePolicy/  {print $NF}'
 
-echo "Accepting node:"
+echo "[relayize:$$] - Accepting node:"
 bash /rudder/bin/accept_pending.sh $UUID || { echo "Failed to accept node!"; exit 1; }
 
-echo "Promote to relay:"
+echo "[relayize:$$] - Promote to relay:"
 /opt/rudder/bin/rudder-node-to-relay $UUID || { echo "Failed to promote!"; exit 1; }
 
-echo "Regenerate promises:"
+echo "[relayize:$$] - Regenerate promises:"
 curl -s http://localhost:8080/rudder/api/deploy/reload; echo "";
+sleep 2
 
 while : ; do
-    echo "Waiting for promises being generated..."
     [[ -f /var/rudder/share/$UUID/rules/cfengine-community/rudder_promises_generated ]] && break
+    echo "[relayize:$$] - Waiting for promises being generated..."
     sleep 15
 done
 grep -q "^$UUID" /var/relayize/relays.txt || { echo "$UUID" >> /var/relayize/relays.txt; }
 
-echo "Exit"
+echo "[relayize:$$]** Exit"
 exit 0
